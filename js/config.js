7d97a121abc3a018f95ebc0976fd1e823003580b
Tyler Powers <gr4yscale@gmail.com>
2015-06-25 19:23:48 +0100

    configure bower packages, sync up require config. use almond AMD loader.

---
+++ js/config.js
@@ -0,0 +1,13 @@
+require.config({
+  shim: {
+
+  },
+  paths: {
+    threejs: "bower_components/threejs/build/three",
+    app: "app",
+    almond: "bower_components/almond/almond"
+  },
+  packages: [
+
+  ]
+});
